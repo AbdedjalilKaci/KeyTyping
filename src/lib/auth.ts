fe2905b4e51a5bb0dd3c1dc3641d8a6723a1fa5e
AbdedjalilKaci <a_kaci@estin.dz>
2025-12-26 23:11:54 +0000

    Fix settings , login and sign up pages

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,8 @@ import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { compare } from 'bcryptjs';
 import { prisma } from './prisma';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
     session: {
         strategy: 'jwt',
     },
